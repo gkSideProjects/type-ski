9c2c9bd818302176ed9010b720545005dc7634c0
George <georgekombo99@hotmail.com>
2020-09-11 21:06:09 +0100

    Added text and input as well as some vue instances for a responsive experience when typing in the input

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,38 @@
+window.onload = function() {
+
+    textToArray(textToType);
+
+}
+
+var textToType = "By the benefit of words and ratiocination they exceed brute beasts in knowledge, and the commodities that accompany the same, so they exceed them also in error.";
+var textArray = [];
+
+// Converts text to array
+function textToArray(text) {
+    textArray = text.split(" ");
+}
+
+
 
 var app = new Vue({
-    el: '#app',
+    el: '#practice',
     data: {
-        product: "about"
+        hide: false
     }
 })
+
+var app2 = new Vue({
+    el: '#input-text',
+    data: {
+        value: "",
+        text: textToType,
+
+    },
+    methods: {
+        clearText() {
+            this.value = "";
+        }
+    }
+})
+
 
