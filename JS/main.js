e7f03910d9673b48d95f6098fb9f9023c564fe40
George <georgekombo99@hotmail.com>
2020-09-17 13:35:46 +0100

    sorted out structure of vue components, still working on a solution to change the style of each worrd in the text array

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,102 @@
+window.onload = function() {
+
+}
 
 var textToType = "By the benefit of words and ratiocination they exceed brute beasts in knowledge, and the commodities that accompany the same, so they exceed them also in error.";
 var textArray = [];
 
-// Converts text to array
-function textToArray(text) {
-    textArray = text.split(" ");
-    return textArray;
-}
-
-
-var app = new Vue({
-    el: '#practice',
-    data: {
-        show: true
+Vue.component('main-content', {
+    template: `
+        <div>
+            <div v-show="show" class="main-menu">
+                <img style="height: 40px; width: 60px;" :src="image">
+                <a href="#" @click="{ clickShow }"><p style="margin: 0; font-size: 30px;">Enter race</p></a>
+            </div>
+            <div v-show="showText" class="practice-div">
+                <div class="text-border">
+                    <p id="text-to-type" class="type-text" v-for="splitWord in splitWords">{{ splitWord }}</p>
+                    <input class="enter-text" v-model="value" @keydown.space="clearText">
+                </div>
+                
+            </div>   
+        </div>
+    `,
+    data() {
+        return {
+            show: true,
+            showText: false,
+            image: "../CONTENT/images/type-ski logo-cut.png",
+            value: "",
+            text: textToType,
+            arrayCount: 0
+        }
     },
     methods: {
-        hideElements() {
-            this.show = false;
-            app2.show = true;
+        clearText(event) {
+            if (this.value === this.splitWords[this.arrayCount] && event.key !== " " && this.value !== "") {
+                return this.arrayCount++;
+            }
+            this.value = "";
+            if (event.key === " ") {
+                event.preventDefault();
+            }
+        }
+    },
+    computed: {
+        clickShow: function() {
+            return (this.show = false, this.showText = true)
+        },
+
+        splitWords: {
+            get: function () {
+                return textArray = this.text.split(" ");
+            },
+            set: function(newTxt) {
+
+            }
         }
     }
 })
 
-var app2 = new Vue({
-    el: '#input-text',
+var app = new Vue   ({
+    el: '#main',
     data: {
-        value: "",
-        letters: textToArray(textToType),
-        show: false,
-        arrayCount: 0,
-        colour: "white",
-        textColour: ""
+    }
+})
+
+/*
+Vue.component('practice-menu', {
+    template: `
+         <div id="practice" v-show="show" class="main-menu">
+            <img style="height: 40px; width: 60px;" :src="image">
+            <a href="#" @click="{ clickShow }"><p style="margin: 0; font-size: 30px;">Enter race</p></a>
+        </div>
+    `,
+    data() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+        clickShow: function() {
+            return this.show = false
+        }
+    }
+})
+
+
+Vue.component('display-text', {
+    template: `
+        <div id="input-text" class="practice-div">
+            <p id="text-to-type" class="type-text" v-for="splitWord in splitWords">{{ splitWord }}</p>
+            <input class="enter-text" v-model="value" @keydown.space="clearText" @keyup="checkText">
+        </div>        
+    `,
+    data() {
+       return {
+
+        }
     },
     methods: {
         clearText(event) {
@@ -40,16 +106,28 @@ var app2 = new Vue({
             }
         },
         checkText(event) {
-            if (this.value.charAt(this.value.length - 1) === textArray[this.arrayCount] && event.key !== " " && this.value !== "") {
-                this.textColour = "lightgreen";
+            if (this.value === this.words[this.arrayCount] && event.key !== " " && this.value !== "") {
                 this.arrayCount++;
+            }
+        }
+    },
+    computed: {
+        splitWords: {
+            get: function () {
+                return textArray = this.text.split(" ");
+            },
+            set: function(newTxt) {
+
             }
         }
     }
 })
+*/
 
 
-window.onload = function() {
-    textToArray(textToType);
 
-}
+
+
+
+
+
