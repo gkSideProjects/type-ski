73bb6e518c8ae81ca7d62d3eaba04199696de444
George <georgekombo99@hotmail.com>
2020-09-18 15:28:21 +0100

    countdown added

--- JS/main.js
+++ JS/main.js
@@ -8,39 +8,64 @@ var textArray = [];
 Vue.component('main-content', {
     template: `
         <div>
-            <div v-show="show" class="main-menu">
+            <div v-show="showMain" class="main-menu">
                 <img style="height: 40px; width: 60px;" :src="image">
                 <a href="#" @click="{ clickShow }"><p style="margin: 0; font-size: 30px;">Enter race</p></a>
             </div>
-            <div v-show="showText" class="practice-div">
+            <div v-show="showPracticeDiv" class="practice-div">
                 <div class="text-border">
-                    <p id="text-to-type" ref="wordRef" class="type-text" v-for="(splitWord) in splitWords">{{ splitWord }}</p>
-                    <input class="enter-text" v-model="value" @keydown.space="clearText">
+                    <p id="text-to-type" v-show="showText"  ref="wordRef" class="type-text" v-for="(splitWord) in splitWords">{{ splitWord }}</p>
+                    <input class="enter-text" @focus="initiateInterval" v-show="showInput" v-model="value" @keydown.space="clearText">
+                    <p>{{ ':' + seconds }}</p>
+                    <p id="information" v-show="showInformation">yo what up</p> 
                 </div>
-                
-            </div>   
+            
+            </div> 
+            <div class="nextRaceDiv" v-show="showButton">
+                <button class="nextRace">next race</button>
+            </div>  
+       
         </div>
     `,
     data() {
         return {
-            show: true,
-            showText: false,
+            showMain: true,
+            showPracticeDiv: false,
+            showText: true,
+            showInput: true,
+            showInformation: false,
+            showButton: false,
             image: "../CONTENT/images/type-ski logo-cut.png",
             value: "",
             text: textToType,
             arrayCount: 0,
-            counter: 0
+            counter: 0,
+            seconds: 150,
+            timeInterval: 0
         }
     },
     methods: {
+        initiateInterval(){
+            this.timeInterval = setInterval(this.timerMethod, 1000)
+        },
+        timerMethod() {
+            this.seconds -= 1
+        },
         clearText(event) {
             if (this.value === this.splitWords[this.arrayCount] && (event.key === " " && this.value !== "")) {
                 this.$refs.wordRef[this.counter].style.color = "#00b300"
                 event.preventDefault();
                 this.counter++
                 this.value = "";
+                if (this.splitWords[this.arrayCount] === this.splitWords[this.splitWords.length - 1]) {
+                    clearInterval(this.timeInterval)
+                    this.showText = false;
+                    this.showInput = false;
+                    this.showButton = true;
+                    this.showInformation = true;
+                }
                 return this.arrayCount++
-            }
+            }   
             this.$refs.wordRef[this.counter].style.color = "indianred"
             if (event.key === " ") {
                 event.preventDefault();
@@ -49,7 +74,7 @@ Vue.component('main-content', {
     },
     computed: {
         clickShow: function() {
-            return (this.show = false, this.showText = true)
+            return (this.showMain = false, this.showPracticeDiv = true)
         },
 
         splitWords: {
@@ -59,7 +84,8 @@ Vue.component('main-content', {
             set: function(newTxt) {
 
             }
-        }
+        },
+
     }
 })
 
