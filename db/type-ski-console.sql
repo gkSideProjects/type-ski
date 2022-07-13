7f5fbed5ce1d9887956960b0e15c88cddd92ee6e
gkSideProjects <71034989+gkSideProjects@users.noreply.github.com>
2022-07-13 23:14:02 +0100

    Update SQL console file

--- db/type-ski-console.sql
+++ db/type-ski-console.sql
@@ -5,20 +5,6 @@ create table users (
     password varchar(255) not null
 );
 
-insert into users (username, password) values('test', 'test');
-
-insert into scores (owner, wpm) values('user1', '105');
-
-select * from scores
-order by wpm desc
-limit 10;
-
-select * from users;
-
-select * from users where username = 'user3' and password = '$2a$10$fDZTZbQxMcV8/XvKPGFDJO7xMKvAk7hLldpHCgE9h0N5gkXKPSgju';
-
-delete from scores where id > 0;
-
 -- leaderboard
 create table scores (
     id serial primary key,
